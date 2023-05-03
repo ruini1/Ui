228053e8048210d84a75e3b6490c98db97466a80
ruini1 <89319877+ruini1@users.noreply.github.com>
2023-05-02 21:37:58 -0400

    Update puppyware.lua

--- puppyware/puppyware.lua
+++ puppyware/puppyware.lua
@@ -338,7 +338,7 @@ function library:new(props)
 		["colorpickers"] = {},
 		["x"] = true,
 		["y"] = true,
-		["key"] = Enum.KeyCode.RightControl,
+		["key"] = Enum.KeyCode.RightShift,
 		["textsize"] = textsize,
 		["font"] = font,
 		["theme"] = {
